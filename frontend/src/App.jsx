6922b8974c4838c4e186574f7c49f3fda1193e6f
shubhiupa19 <s.x.upadhyay@gmail.com>
2024-09-20 19:22:23 -0400

    worked on routes + seeing if page works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
+import StudentSignIn from './pages/StudentSignIn';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage/>} />
-          <Route path="/create-account" element={<CreateAccount/>} />
-          <Route path="/student-signup" element={<StudentSignIn/>} />
-          <Route path = "tutor-signup" element={<TutorSignIn/>} />
-          <Route path = "teacher-signup" element={<TeacherSignIn/>} />
-          <Route path = "admin-signup" element={<AdminSignIn/>} />
-          <Route path = "student-dashboard" element={<StudentDashboard/>} />
-          <Route path = "tutor-dashboard" element={<TutorDashboard/>} />
-          <Route path = "admin-dashboard" element={<AdminDashboard/>} />
+           {/* <Route path="/" element={<LoginPage/>} />
+          <Route path="/create-account" element={<CreateAccount/>} />  */}
+          {/* <Route path="/student-signup" element={<StudentSignIn/>} />  */}
+          {/* <Route path = "/tutor-signup" element={<TutorSignIn/>} />
+          <Route path = "/teacher-signup" element={<TeacherSignIn/>} />
+          <Route path = "/admin-signup" element={<AdminSignIn/>} />
+          <Route path = "/student-dashboard" element={<StudentDashboard/>} />
+          <Route path = "/tutor-dashboard" element={<TutorDashboard/>} />
+          <Route path = "/admin-dashboard" element={<AdminDashboard/>} />  */}
 
          
          </Routes>
---
+++ frontend/src/pages/StudentSignIn.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function StudentSignIn() {
+    return (
+        <div>
+            <h1>Student Sign In</h1>
+        </div>
+    )
+};
+
+
+
+export default StudentSignIn;
