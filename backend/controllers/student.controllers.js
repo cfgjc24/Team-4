986132497ed540762f71db6f4ca9897f6c0932b2
patriciaacw <patriciacolumbiawalsh@gmail.com>
2024-09-20 22:12:50 -0400

    test

--- backend/controllers/student.controllers.js
+++ backend/controllers/student.controllers.js
@@ -11,23 +11,6 @@ export const getStudents = async (req, res) => {
     }
 };
 
-<<<<<<< HEAD
-export const createStudent = async (req, res) => {
-
-    const student = req.body; // user sends this data
-    if (!student.name || !student.price || !student.image) {
-        return res.status(400).json({success:false, message: "Please provide all fields"})
-    }
-
-    const newStudent = new Student(student) // create new student with info given
-
-    try {
-        await newStudent.save();
-        res.status(201).json({sucess: true, data: newStudent});
-    } catch (error) {
-        console.error("Error in Create Student: ", error.message);
-        res.status(500).json({ sucess: false, message: "Server Error"});
-=======
 export const getStudent = async (req, res) => {
     const { email } = req.params; 
     try {
@@ -105,26 +88,16 @@ export const createStudent = async (req, res) => {
     } catch (error) {
         console.log("Error in creating student: ", error.message);
         res.status(500).json({ message: "Server error: " + error.message });
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
     }
 };
 
 export const deleteStudent = async (req, res) => {
-<<<<<<< HEAD
-    const { id } = req.params;
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({success: false, message: "Invalid student ID"});
-    }
-    try {
-        await Student.findByIdAndDelete(id);
-=======
     const { email } = req.params;
     if(!mongoose.Types.ObjectId.isValid(email)){
         return res.status(404).json({success: false, message: "Invalid student email"});
     }
     try {
         await Student.findByIdAndDelete(email);
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
         res.status(200).json( { success: true, message: "successfully deleted"});
     } catch (error) {
         res.status(500).json( { success: false, message: "Server Error"});
@@ -132,15 +105,6 @@ export const deleteStudent = async (req, res) => {
 };
 
 export const updateStudent = async (req, res) => {
-<<<<<<< HEAD
-    const { id } = req.params;
-    const student = req.body;
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({success: false, message: "Invalid student ID"});
-    }
-    try {
-        const updatedStudent = await Student.findByIdAndUpdate(id, student, {new:true});
-=======
     const { email } = req.params;
     const student = req.body;
     if(!mongoose.Types.ObjectId.isValid(email)){
@@ -148,7 +112,6 @@ export const updateStudent = async (req, res) => {
     }
     try {
         const updatedStudent = await Student.findByIdAndUpdate(email, student, {new:true});
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
         res.status(200).json({sucess:true, data: updatedStudent});
     } catch (error) {
         res.status(500).json({ success: false, message: "Server Error"});
--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -1,22 +1,11 @@
 import express from "express";
 
-<<<<<<< HEAD
-import { createStudent, getStudents, updateStudent, deleteStudent } from '../controllers/student.controllers.js';
-=======
 import { createStudent, getStudents, updateStudent, deleteStudent, getStudent } from '../controllers/student.controllers.js';
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
 
 const router = express.Router();
 
 router.get("/", getStudents);
 
-<<<<<<< HEAD
-router.post("/", createStudent);
-
-router.delete("/:id", deleteStudent);
-
-router.put("/:id", updateStudent);
-=======
 router.get("/:email", getStudent);
 
 router.post("/", createStudent); // student create account
@@ -24,6 +13,5 @@ router.post("/", createStudent); // student create account
 router.delete("/:email", deleteStudent); // admin functionality
 
 router.put("/:email", updateStudent); // student profile
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
 
 export default router;
