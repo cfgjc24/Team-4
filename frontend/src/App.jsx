e41845ff85cbfef2aca6794d2e4519f52e2cfe40
shubhiupa19 <s.x.upadhyay@gmail.com>
2024-09-20 23:08:26 -0400

    committed changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,11 @@ import StudentSignIn from './pages/StudentSignIn';
 import LogIn from './pages/LogIn';
 import CreateAccount from './pages/CreateAccount';
 import TutorSignIn from './pages/TutorSignIn';
-
+import ButtonAppBar from './pages/TutorHomePage';
+import AdminDashboard from './pages/AdminDashboard';
+import CourseMaterials from './pages/CourseMaterials';
+import './App.css';
+import './index.css';
 
 function App() {
   return (
@@ -16,15 +20,17 @@ function App() {
             <Route path = "/tutor-home-page" element={<ButtonAppBar/>} />
          {/* <Route path="/create-account" element={<CreateAccount/>} />  */}
           {/* <Route path="/student-signup" element={<StudentSignIn/>} />  */}
-           <Route path = "/tutor-signup" element={<TutorSignIn/>} />
+           {/* <Route path = "/tutor-signup" element={<TutorSignIn/>} /> */}
+          <Route path="/" element={<LogIn/>} /> 
+
          {/* <Route path = "/teacher-signup" element={<TeacherSignIn/>} />
           <Route path = "/admin-signup" element={<AdminSignIn/>} />
           <Route path = "/student-dashboard" element={<StudentDashboard/>} />
-          <Route path = "/tutor-dashboard" element={<TutorDashboard/>} />
-          <Route path = "/admin-dashboard" element={<AdminDashboard/>} />  */}
-
-         
-         </Routes>
+          <Route path = "/tutor-dashboard" element={<TutorDashboard/>} /> */}
+          <Route path="/CourseMaterials" element={<CourseMaterials/>} />
+          <Route path="/AdminDashboard" element={<AdminDashboard />} />
+          <Route path="/CourseMaterials" element={<CourseMaterials />} />
+        </Routes>
       </Router>
     </>
   );
