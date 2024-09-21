6fc9c7152fec4657c63deac0c63002eb9d5f2db0
Mahika Calyanakoti <mahika@seas.upenn.edu>
2024-09-20 23:13:49 -0400

    chapters routes

--- backend/routes/chapter.routes.js
+++ backend/routes/chapter.routes.js
@@ -4,14 +4,14 @@ import { createChapter, getChapters, updateChapter, deleteChapter, getChapter } 
 
 const router = express.Router();
 
-router.get("/", getChapter);
+router.get("/", getChapters);
 
-router.get("/:email", getChapter);
+router.get("/:name", getChapter);
 
 router.post("/", createChapter); // student create account
 
-router.delete("/:email", deleteChapter); // admin functionality
+router.delete("/:name", deleteChapter); // admin functionality
 
-router.put("/:email", updateChapter); // student profile
+router.put("/:name", updateChapter); // student profile
 
 export default router;
