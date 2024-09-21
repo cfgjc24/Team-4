c94f3940955d716d5902c975faf6ffd82292c918
dejadunlap <70656325+dejadunlap@users.noreply.github.com>
2024-09-21 03:44:54 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import studentRoutes from "./routes/student.routes.js";
+import metricRoutes from './routes/metrics.routes.js';  // Make sure this path is correct
+
 
 // allows us to see environment info
 dotenv.config()
@@ -10,7 +12,9 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // middle layer that allows us to parse json data in body
 
-app.use("/api/students", studentRoutes); // set prefix for all route methods
+
+app.use("/api/students", studentRoutes);
+app.use("/api/metrics/", metricRoutes); // set prefix for all route methods
 
 // prints message to homepage
 app.get("/", (req, res) => {
