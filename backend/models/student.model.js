f27faa41a0877250f53aa3c0a627f527e1f7e447
Mahika Calyanakoti <mahika@seas.upenn.edu>
2024-09-20 20:10:18 -0400

    updated students schema

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -19,63 +19,79 @@ const studentSchema = new mongoose.Schema({
         required: true
     },
     start_week: {
-        type: Date // Assuming start week is a date field
+        type: Date, // Assuming start week is a date field
+        required: true
     },
     graduated: {
         type: Boolean,
-        default: false
+        default: false,
+        required: true
     },
     race: {
         type: String,
-        enum: ['Asian', 'Black', 'Hispanic', 'White', 'Other'] 
+        enum: ['Asian', 'Black', 'Hispanic', 'White', 'Other'],
+        required: true
     },
     ethnicity: {
         type: String,
-        enum: ['Non-Hispanic', 'Hispanic', 'Unknown'] 
+        enum: ['Non-Hispanic', 'Hispanic', 'Unknown'],
+        required: true 
     },
     gender: {
         type: String,
-        enum: ['Male', 'Female', 'Non-Binary', 'Other']
+        enum: ['Male', 'Female', 'Non-Binary', 'Other'],
+        required: true
     },
     pronouns: {
         type: String,
-        enum: ['He/Him', 'She/Her', 'They/Them', 'Other']
+        enum: ['He/Him', 'She/Her', 'They/Them', 'Other'],
+        required: true
     },
     town: {
-        type: String
+        type: String,
+        required: true
     },
     state: {
-        type: String
+        type: String,
+        required: true
     },
     high_school: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'HighSchool' // Reference to HighSchool model
+        ref: 'HighSchool', // Reference to HighSchool model
+        required: true
     },
-    high_school: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Teacher' // Reference to HighSchool model
+    teacher_email: {
+        type: String,
+        required: true
     },
     date_of_birth: {
-        type: Date
+        type: Date,
+        required: true
     },
     phone_number: {
-        type: String
+        type: String,
+        required: true
     },
     active: {
         type: Boolean,
-        default: false
+        default: false,
+        required: true
     },
     complete: {
         type: Boolean,
-        default: false
+        default: false,
+        required: true
     },
     w1_through_w8_attendance: {
         type: [Boolean], // array of booleans for each week
-        validate: [arrayLimit, '{PATH} exceeds the limit of 8'] // custom validator for array length
+        default: [false, false, false, false, false, false, false, false],
+        validate: [arrayLimit, '{PATH} exceeds the limit of 8'], // custom validator for array length
+        required: true
     },
     capstone: {
         type: Boolean,
-        default: false
+        default: false,
+        required: true
     }
 }, {
     timestamps: true // createdAt, updatedAt
