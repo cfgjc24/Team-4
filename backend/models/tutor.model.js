b474f314ba59c3e7182fc4ca23bbe970acb3449a
Mahika Calyanakoti <mahika@seas.upenn.edu>
2024-09-21 00:57:15 -0400

    updated controlers and models

--- backend/models/tutor.model.js
+++ backend/models/tutor.model.js
@@ -26,11 +26,13 @@ const tutorSchema = new mongoose.Schema({
     },
     race: {
         type: String,
+        enum: ['Asian', 'Black', 'Hispanic', 'White', 'Other'],
         required: true
     },
     ethnicity: {
         type: String,
-        required: true
+        enum: ['Non-Hispanic', 'Hispanic', 'Unknown'],
+        required: true 
     },
     gender: {
         type: String,
