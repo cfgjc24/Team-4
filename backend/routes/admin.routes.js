a71e2343cd84c24606794c679a55c97bb9c59b60
patriciaacw <patriciacolumbiawalsh@gmail.com>
2024-09-20 22:56:45 -0400

    create routes and controller for admin

---
+++ backend/routes/admin.routes.js
@@ -0,0 +1,28 @@
+// routes/admin.routes.js
+import express from "express";
+import {
+  createAdmin,
+  getAdmins,
+  getAdminByEmail,
+  updateAdmin,
+  deleteAdmin,
+} from "../controllers/admin.controller.js";
+
+const router = express.Router();
+
+// Route to get all admins
+router.get("/", getAdmins);
+
+// Route to get a specific admin by email
+router.get("/:email", getAdminByEmail);
+
+// Route to create a new admin
+router.post("/", createAdmin);
+
+// Route to update an admin by email
+router.put("/:email", updateAdmin);
+
+// Route to delete an admin by email
+router.delete("/:email", deleteAdmin);
+
+export default router;
