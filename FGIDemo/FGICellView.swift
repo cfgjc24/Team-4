d9a14107a23f3217283a6847543a6b72353ce446
Ibrahima Toure <145729747+IbrahimaST@users.noreply.github.com>
2024-09-21 02:49:22 -0400

    Mobile Implementation
    created SwiftUI version of the app

---
+++ FGIDemo/FGICellView.swift
@@ -0,0 +1,22 @@
+//
+//  FGIOrgzDetailView.swift
+//  FGIDemo
+//
+//  Created by Ibrahima Toure on 9/20/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct FGICellView: View {
+    
+    var course: CourseFramework
+    
+    var body: some View {
+        Text("Placeholder")
+    }
+}
+                        
+#Preview {
+    FGICellView(course: CourseMaterials.sampleCourse)
+}
