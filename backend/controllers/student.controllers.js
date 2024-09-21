51c35aab3aeaf9ef3225bbb32f95396f2a99e361
dejadunlap <70656325+dejadunlap@users.noreply.github.com>
2024-09-20 22:13:27 -0400

    merge

--- backend/controllers/student.controllers.js
+++ backend/controllers/student.controllers.js
@@ -11,9 +11,6 @@ export const getStudents = async (req, res) => {
     }
 };
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 export const createStudent = async (req, res) => {
 
     const student = req.body; // user sends this data
@@ -29,8 +26,7 @@ export const createStudent = async (req, res) => {
     } catch (error) {
         console.error("Error in Create Student: ", error.message);
         res.status(500).json({ sucess: false, message: "Server Error"});
-=======
->>>>>>> 14e049da0ba1c30e9ad0f7cca824a2d0e3157372
+
 export const getStudent = async (req, res) => {
     const { email } = req.params; 
     try {
@@ -108,35 +104,22 @@ export const createStudent = async (req, res) => {
     } catch (error) {
         console.log("Error in creating student: ", error.message);
         res.status(500).json({ message: "Server error: " + error.message });
-<<<<<<< HEAD
-=======
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
->>>>>>> 14e049da0ba1c30e9ad0f7cca824a2d0e3157372
     }
 };
 
 export const deleteStudent = async (req, res) => {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
     const { id } = req.params;
     if(!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({success: false, message: "Invalid student ID"});
     }
     try {
         await Student.findByIdAndDelete(id);
-=======
->>>>>>> 14e049da0ba1c30e9ad0f7cca824a2d0e3157372
     const { email } = req.params;
     if(!mongoose.Types.ObjectId.isValid(email)){
         return res.status(404).json({success: false, message: "Invalid student email"});
     }
     try {
         await Student.findByIdAndDelete(email);
-<<<<<<< HEAD
-=======
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
->>>>>>> 14e049da0ba1c30e9ad0f7cca824a2d0e3157372
         res.status(200).json( { success: true, message: "successfully deleted"});
     } catch (error) {
         res.status(500).json( { success: false, message: "Server Error"});
@@ -144,9 +127,6 @@ export const deleteStudent = async (req, res) => {
 };
 
 export const updateStudent = async (req, res) => {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
     const { id } = req.params;
     const student = req.body;
     if(!mongoose.Types.ObjectId.isValid(id)){
@@ -154,8 +134,6 @@ export const updateStudent = async (req, res) => {
     }
     try {
         const updatedStudent = await Student.findByIdAndUpdate(id, student, {new:true});
-=======
->>>>>>> 14e049da0ba1c30e9ad0f7cca824a2d0e3157372
     const { email } = req.params;
     const student = req.body;
     if(!mongoose.Types.ObjectId.isValid(email)){
@@ -163,10 +141,6 @@ export const updateStudent = async (req, res) => {
     }
     try {
         const updatedStudent = await Student.findByIdAndUpdate(email, student, {new:true});
-<<<<<<< HEAD
-=======
->>>>>>> 846f8410ccf882b5c439cb8a4108f86aa460c010
->>>>>>> 14e049da0ba1c30e9ad0f7cca824a2d0e3157372
         res.status(200).json({sucess:true, data: updatedStudent});
     } catch (error) {
         res.status(500).json({ success: false, message: "Server Error"});
