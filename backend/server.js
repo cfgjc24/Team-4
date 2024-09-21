b0658c4abaedb0f9b0eb8ab8e189ffd8deaa4ed7
Mahika Calyanakoti <mahika@seas.upenn.edu>
2024-09-20 23:23:50 -0400

    updated server js with apis

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+
 import studentRoutes from "./routes/student.routes.js";
+import teacherRoutes from "./routes/teacher.routes.js";
+import tutorRoutes from "./routes/tutor.routes.js";
+import chapterRoutes from "./routes/chapter.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import highschoolRoutes from "./routes/highschool.routes.js";
+
 
 // allows us to see environment info
 dotenv.config()
@@ -11,10 +18,12 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json()); // middle layer that allows us to parse json data in body
 
 app.use("/api/students", studentRoutes); // set prefix for all route methods
-app.use("/api/teachers", studentRoutes); // set prefix for all route methods
-app.use("/api/tutors", studentRoutes); // set prefix for all route methods
-app.use("/api/chapters", studentRoutes); // set prefix for all route methods
-app.use("/api/admin", studentRoutes); // set prefix for all route methods
+app.use("/api/teachers", teacherRoutes); // set prefix for all route methods
+app.use("/api/tutors", tutorRoutes); // set prefix for all route methods
+app.use("/api/chapters", chapterRoutes); // set prefix for all route methods
+app.use("/api/admins", adminRoutes); // set prefix for all route methods
+app.use("/api/highschools", highschoolRoutes); // set prefix for all route methods
+
 
 // prints message to homepage
 app.get("/", (req, res) => {
